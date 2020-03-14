7072c8798e10212dd645e786e1aa1d936248f2a0
Joule <4247532@gmail.com>
2020-03-14 10:52:11 -0400

    PATHFINDING FOR PARTNER!!! OwO

---
+++ Assets/Scripts/PartnerMove.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PartnerMove : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform _destination;
+
+    NavMeshAgent _navMeshAgent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if (_navMeshAgent == null)
+        {
+            Debug.Log("Not good nav mesh");
+        }
+        else
+        {
+            SetDestination();
+        }
+    }
+
+    private void SetDestination()
+    {
+        if (_destination != null)
+        {
+            Vector3 targetVector = _destination.transform.position;
+            _navMeshAgent.SetDestination(targetVector);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

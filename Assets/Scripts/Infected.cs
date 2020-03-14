1bef6fb3067280eee7fe333a12795a4b96e123cd
Joule <4247532@gmail.com>
2020-03-14 19:45:17 -0400

    Fix?

---
+++ Assets/Scripts/Infected.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Infected : MonoBehaviour
+{
+    public float LVinfect = 0f;
+    public int cures = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PartnerMove.cs
+++ Assets/Scripts/PartnerMove.cs
@@ -39,7 +39,7 @@ public class PartnerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Go)
+        if (true)
         {
             Go = false;
             SetDestination(_destination);

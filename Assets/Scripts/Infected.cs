49afe5e6fa11fba1c54c9277fe87dc4a4de71467
Joule <4247532@gmail.com>
2020-03-15 18:31:24 -0400

    Version presque fini

--- Assets/Scripts/Infected.cs
+++ Assets/Scripts/Infected.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Infected : MonoBehaviour
 {
     public float LVinfect = 0f;
     public int cures = 0;
     public int maxCures = 5;
-    public MonoBehaviour slider;
+    public Slider slider;
+    public Text text;
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -15,23 +18,42 @@ public class Infected : MonoBehaviour
             if (cures <= 0)
             {
                 LVinfect += 10f;
-
+                SetInfectLV(LVinfect);
                 collision.collider.GetComponent<PartnerMove>().die();
             }
             else
             {
                 cures--;
+                SetCureNum(cures);
                 collision.collider.GetComponent<PartnerMove>().live();
             }
         }
+    }
+    
+
+    public void SetInfectLV(float lv)
+    {
+        lv = Mathf.Clamp(lv, 0f, 100f);
+        slider.value = lv;
+    }
+    public void SetCureNum(int c)
+    {
+        text.text = c + "x";
     }
 
     public void GetCure()
     {
         if (cures < maxCures)
         {
-            cures++;
+            cures+=3;
+            SetCureNum(cures);
         }
+    }
+
+    private void Start()
+    {
+        SetInfectLV(0f);
+        SetCureNum(0);
     }
 
     private void Update()

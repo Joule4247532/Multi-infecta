0cca964b90a7aed7afa77cfc6aa3a513b37ea124
Dyke10 <Dyke10.yt@gmail.com>
2020-03-15 13:54:01 -0500

    Added Real Menu

---
+++ Assets/Scripts/Menu_about.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_about : MonoBehaviour
+{
+    public GameObject About2;
+    private void Start()
+    {
+        About2 = GameObject.Find("Menu_about2");
+        About2.GetComponent<Renderer>().enabled = false;
+    }
+    void OnMouseOver()
+    {
+        //gameObject.SetActive(false);
+        gameObject.GetComponent<Renderer>().enabled = false;
+        About2.GetComponent<Renderer>().enabled = true;
+    }
+
+
+    void OnMouseExit()
+    {
+        gameObject.GetComponent<Renderer>().enabled = true;
+        About2.GetComponent<Renderer>().enabled = false;
+    }
+}

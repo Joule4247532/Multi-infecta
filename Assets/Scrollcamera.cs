3688cfa1f0757506e10cc0f5be5ef4d81b52f793
Dyke10 <Dyke10.yt@gmail.com>
2020-03-14 21:23:04 -0500

    Sprites and scrollzoom script

---
+++ Assets/Scrollcamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrollcamera : MonoBehaviour
+{
+    public float Y = 0; // set this to 0.2
+    public float B = 0; // set this to -0.2
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            transform.Translate(0, Y, -1);
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            transform.Translate(0, B, 1);
+        }
+    }
+}

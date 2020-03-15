9ead0c23ac0aab68890b48c4567429237c1b894e
Joule <4247532@gmail.com>
2020-03-15 02:21:36 -0400

    Spawning Patients

--- Assets/Scripts/PlayerMouvement.cs
+++ Assets/Scripts/PlayerMouvement.cs
@@ -4,10 +4,16 @@ public class PlayerMouvement : MonoBehaviour
 {
 
     public Rigidbody rb;
+    public Transform tr;
     public float speed = 20f;
     private float trueSpeed;
     public bool col = false;
+    private float angle;
 
+    private void Start()
+    {
+        angle = 0f;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -24,21 +30,27 @@ public class PlayerMouvement : MonoBehaviour
         if (Input.GetKey("a") && !Input.GetKey("d"))
         {
             rb.position += new Vector3(-trueSpeed / 1000, 0, 0);
+            angle = 270f;
         }
         if (Input.GetKey("d") && !Input.GetKey("a"))
         {
             rb.position += new Vector3(trueSpeed / 1000, 0, 0);
+            angle = 90f;
         }
         if (Input.GetKey("w") && !Input.GetKey("s"))
         {
             rb.position += new Vector3(0, 0, trueSpeed / 1000);
+            angle = 0f;
         }
         if (Input.GetKey("s") && !Input.GetKey("w"))
         {
             rb.position += new Vector3(0, 0, -trueSpeed / 1000);
+            angle = 180f;
         }
 
         rb.position = new Vector3(rb.position.x,0.64f,rb.position.z);
+        tr.rotation = Quaternion.Euler(-90f, angle, 0f);
+        rb.velocity = new Vector3(0f, 0f, 0f);
     }
 
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -18,10 +18,14 @@ public class Spawner : MonoBehaviour
     public Wave[] waves;
     private int nextWave = 0;
 
-    public float waveTimer = 5f;
-    public float waveCountdown;
+    public Transform[] spawnPoints;
 
-    public SpawnState state = SpawnState.COUNTING;
+    public float waveTimer = 5f;
+    private float waveCountdown;
+
+    private float searchTimer = 1f;
+
+    private SpawnState state = SpawnState.COUNTING;
 
     void Start()
     {
@@ -30,9 +34,87 @@ public class Spawner : MonoBehaviour
 
     void Update()
     {
+        if (state == SpawnState.WAITING)
+        {
+            if (!EnemyIsAlive())
+            {
+                NextWave();
+            }
+            else
+            {
+                return;
+            }
+        }
+        
         if (waveCountdown <= 0)
         {
-
+            if (state != SpawnState.SPAWNING)
+            {
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
         }
+        else
+        {
+            waveCountdown -= Time.deltaTime;
+        }
+    }
+
+    void NextWave()
+    {
+
+        state = SpawnState.COUNTING;
+        waveCountdown = waveTimer;
+
+        if (nextWave + 1 > waves.Length - 1)
+        {
+            nextWave = 0;
+            //End of waves
+        }
+        else
+        {
+            nextWave++;
+        }
+    }
+
+    bool EnemyIsAlive()
+    {
+        searchTimer -= Time.deltaTime;
+        if (searchTimer <= 0f)
+        {
+            searchTimer = 1f;
+
+            if (GameObject.FindGameObjectsWithTag("Patients").Length == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    IEnumerator SpawnWave(Wave _wave)
+    {
+        state = SpawnState.SPAWNING;
+
+        for (int i = 0; i < _wave.count; i++)
+        {
+            SpawnPatient(_wave.enemy);
+            yield return new WaitForSeconds(1f / _wave.rate);
+        }
+
+        state = SpawnState.WAITING;
+
+        yield break;
+    }
+
+    void SpawnPatient(Transform _patient)
+    {
+        if (spawnPoints.Length == 0)
+        {
+            Debug.LogError("No Spawn Points");
+            return;
+        }
+        Transform _sp = spawnPoints[ Random.Range(0,spawnPoints.Length)];
+        Instantiate(_patient, _sp.position, _sp.rotation);
+        Debug.Log("Spawning patient " + _patient.name);
     }
 }

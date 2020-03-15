d209d07a4248122302107e08d24281abd1954302
Joule <4247532@gmail.com>
2020-03-15 12:31:35 -0400

    kk

---
+++ Assets/Scripts/CureManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class CureManager : MonoBehaviour
+{
+    public GameObject cure;
+    private Transform[] spawns;
+    public Vector2 spawnDelayMinMax = new Vector2(2f,5f);
+    private float timer = 0f;
+
+
+    private void Start()
+    {
+        spawns = this.GetComponentsInChildren<Transform>();
+    }
+
+    private void Update()
+    {
+        Debug.Log(timer);
+        if (timer <= 0f)
+        {
+            timer = UnityEngine.Random.Range(spawnDelayMinMax.x, spawnDelayMinMax.y);
+            SpawnCure();
+        }
+        timer -= Time.deltaTime;
+    }
+
+    private void SpawnCure()
+    {
+        throw new NotImplementedException();
+    }
+}

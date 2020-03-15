0338a8bc42a7ee5b3ff1bc45f096d42c2df611bf
Joule <4247532@gmail.com>
2020-03-14 23:43:18 -0400

    progress

--- Assets/Scripts/Scrollcamera.cs
+++ Assets/Scripts/Scrollcamera.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Scrollcamera : MonoBehaviour
 {
-    public float Y = 0; // set this to 0.2
-    public float B = 0; // set this to -0.2
+    public float Y = 0.2f; // set this to 0.2
+    public float B = -0.2f; // set this to -0.2
+    private float amount = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,13 +16,15 @@ public class Scrollcamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        if (Input.GetAxis("Mouse ScrollWheel") < 0 && amount <= 2)
         {
             transform.Translate(0, Y, -1);
+            amount += Y;
         }
-        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && amount >= -2)
         {
             transform.Translate(0, B, 1);
+            amount += B;
         }
     }
 }
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+
+    public enum SpawnState { SPAWNING, WAITING, COUNTING };
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public float waveTimer = 5f;
+    public float waveCountdown;
+
+    public SpawnState state = SpawnState.COUNTING;
+
+    void Start()
+    {
+        waveCountdown = waveTimer;
+    }
+
+    void Update()
+    {
+        if (waveCountdown <= 0)
+        {
+
+        }
+    }
+}

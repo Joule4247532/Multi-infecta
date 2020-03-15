f9395432a7d69e9cda5ec6d42691b9d2d9776406
Dyke10 <Dyke10.yt@gmail.com>
2020-03-15 16:38:21 -0500

    kk

--- Assets/Scripts/Menu_play.cs
+++ Assets/Scripts/Menu_play.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 public class Menu_play : MonoBehaviour
 {
     public GameObject Menu2;
+    public string Testing;
     private void Start()
     {
         Menu2 = GameObject.Find("Menu_play2");
         Menu2.GetComponent<Renderer>().enabled = false;
+    }
+
+    public void OnMouseDown()
+    {
+        SceneManager.LoadScene(Testing);
     }
     void OnMouseOver()
     {
@@ -15,7 +22,6 @@ public class Menu_play : MonoBehaviour
         gameObject.GetComponent<Renderer>().enabled = false;
         Menu2.GetComponent<Renderer>().enabled = true;
     }
-
 
     void OnMouseExit()
     {

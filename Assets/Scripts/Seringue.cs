58d02a60a143a31116b7f957e91f0cf4157fa81f
Dyke10 <Dyke10.yt@gmail.com>
2020-03-16 14:45:50 -0500

    CONFLICT ^^^^^

--- Assets/Scripts/Seringue.cs
+++ Assets/Scripts/Seringue.cs
@@ -3,13 +3,20 @@
 public class Seringue : MonoBehaviour
 {
     public float animSpeed = 0.2f;
+    public GameObject audioManager;
 
+
+    private void Start()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("audiomanager");
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             other.GetComponent<Infected>().GetCure();
             DeleteSelf();
+           audioManager.GetComponent<AudioManager>().Play("SFX");
         }
     }
 

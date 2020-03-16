151f127b7e35f473ef14cc1ba86fb8a068783179
Joule <4247532@gmail.com>
2020-03-15 23:21:45 -0400

    DataManagement

---
+++ Assets/Scripts/GameOverData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverData : MonoBehaviour
+{
+    public Text scoreBoard;
+    public Text patientBoard;
+    public Text timeBoard;
+    public GameObject data;
+
+    void Start()
+    {
+        scoreBoard.text = "Score: " + data.GetComponent<StaticData>().score.ToString();
+        patientBoard.text = "Cured patients: " + data.GetComponent<StaticData>().cured.ToString();
+        timeBoard.text = "Time Played: " + data.GetComponent<StaticData>().gameTime.ToString();
+    }
+}

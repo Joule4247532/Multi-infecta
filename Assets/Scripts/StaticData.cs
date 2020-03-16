98f713fd8706f49a089cf78a2ba3c442f7fcd6e1
Joule <4247532@gmail.com>
2020-03-15 23:19:06 -0400

    Revert "Music"
    This reverts commit 95628dc253ffeab786be29a4fc858dbb0fe48cce.

---
+++ Assets/Scripts/StaticData.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class StaticData : MonoBehaviour
+{
+    static int curedStatic = 0;
+    static float gameTimeStatic = 0f;
+    public int score = 0;
+    public int cured = 0;
+    public float gameTime = 0f;
+
+    private void Awake()
+    {
+        cured = curedStatic;
+        gameTime = gameTimeStatic;
+        score = (curedStatic * 20) - (int)(gameTimeStatic);
+    }
+
+    public void storeData()
+    {
+        curedStatic = cured;
+        gameTimeStatic = gameTime;
+    }
+}

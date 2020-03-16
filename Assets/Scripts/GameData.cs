0a0eb600fc7a40bbbcfb4a1927b8c1e44815234f
Joule <4247532@gmail.com>
2020-03-16 13:10:23 -0400

    update 1

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -6,7 +6,7 @@ public class GameData : MonoBehaviour
     private int cured;
     private float timeStart;
 
-    private void Awake()
+    private void Start()
     {
         timeStart = Time.time;
     }

0a437f6309ec3bb4b55c50dcce5e4db934a0b545
Dyke10 <Dyke10.yt@gmail.com>
2020-03-15 19:24:06 -0500

    Added end fix n.2

---
+++ Assets/Scripts/End_back.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End_back : MonoBehaviour
+{
+    string Testing = "Testing";
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(Testing);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
+
+
